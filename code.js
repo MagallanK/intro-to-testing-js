4a843d6f8f9aefaded4041dbefeb12b029cbcfaa
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-08-24 13:51:02 -0500

    Exercise #4 'Add another small, simple test' -Asserting Alex

--- code.js
+++ code.js
@@ -3,11 +3,12 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-// sayHello function
-// function sayHello() {
-//     return "Say, Hello!";
-// }
+//sayHello function
+function sayHello() {
+    return "Say, Hello!";
+}
 
+//sayHello, Jane, Alex
 function sayHello(name) {
     if (name === undefined) {
         return "Say, Hello!";
